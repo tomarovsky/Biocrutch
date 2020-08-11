0bf220de6b77abd87455993fb6f82934ce37ab3e
etozhetoma <etozhetoma@ya.ru>
2020-08-11 18:02:14 +0300

    added ema_bin_to_fastq.py

---
+++ scripts/ema_bin_to_fastq.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+
+def main():
+    for file in args.input:
+        fh = open(file, "r")
+        for line in fh:
+            line = line.split()
+            with open("barcodes.txt", 'a') as f:
+                f.write(line[0] + '\n')
+            with open(os.path.splitext(file)[0] + "_1.fastq", 'a') as f:
+                f.write(line[1] + '\n')
+                f.write(line[2] + '\n')
+                f.write('+\n')
+                f.write(line[3] + '\n')
+            with open(os.path.splitext(file)[0] + "_2.fastq", 'a') as f:
+                f.write(line[1] + '\n')
+                f.write(line[4] + '\n')
+                f.write('+\n')
+                f.write(line[5] + '\n')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ema-bin-xxx to forward_1.fastq, reverse_2.fastq and barcodes.txt")
+    group_required = parser.add_argument_group('Options')
+    group_required.add_argument('-i', '--input', type=str,
+                            nargs='+', help="input file ema-bin-xxx")
+    args = parser.parse_args()
+    main()
+
+
