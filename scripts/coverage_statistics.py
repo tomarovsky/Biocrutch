0814dc6c93a39d91b561a799f3a8847ce18d2f62
etozhetoma <etozhetoma@ya.ru>
2020-10-09 17:01:17 +0300

    fixes + removed extra functions

--- scripts/coverage_statistics.py
+++ scripts/coverage_statistics.py
@@ -14,64 +14,6 @@ import pandas as pd
 import argparse
 
 
-def scaffold_stats(scaffold_coverages_dict: defaultdict, scaffold_name: str) -> list:
-    # use to calculate stats for each scaffold
-    tmp_lst_to_df = []
-    # median
-    scaffold_list_of_coverages = sorted(scaffold_coverages_dict[scaffold_name])
-    if len(scaffold_coverages_dict[scaffold_name]) % 2 != 0:
-        index = int(len(scaffold_coverages_dict[scaffold_name]) / 2)
-        tmp_lst_to_df.append(scaffold_list_of_coverages[index])
-    else:
-        index_1 = int(len(scaffold_coverages_dict[scaffold_name])/2 - 0.5)
-        value_1 = scaffold_list_of_coverages[index_1]
-        index_2 = int(len(scaffold_coverages_dict[scaffold_name])/2 - 0.5)
-        value_2 = scaffold_list_of_coverages[index_2]
-        tmp_lst_to_df.append((value_1 + value_2) / 2)
-    # average, max, min
-    scaffold_line_counter = len(scaffold_coverages_dict[scaffold_name])
-    tmp_lst_to_df.append(sum(scaffold_coverages_dict[scaffold_name])/scaffold_line_counter)
-    tmp_lst_to_df.append(max(scaffold_coverages_dict[scaffold_name]))
-    # .pop() to clear the dictionary
-    tmp_lst_to_df.append(min(scaffold_coverages_dict.pop(scaffold_name)))
-    return tmp_lst_to_df
-
-
-def frame_stats(coverages_amounts_dict: Counter, line_counter: int) -> list:
-    # use to calculate stats for stacking windows and whole genome
-    sum_of_coverages = 0
-    for key, value in coverages_amounts_dict.items():
-        sum_of_coverages += key * value
-
-    # median
-    keys_coverages = sorted(coverages_amounts_dict.keys())
-    sum_values_coverages = sum(coverages_amounts_dict.values())
-    half_sum_values_coverages = sum_values_coverages / 2
-    count = 0
-
-    if sum_values_coverages % 2 != 0:
-        for i in range(len(keys_coverages)):
-            count += coverages_amounts_dict[keys_coverages[i]]
-            if count >= half_sum_values_coverages:
-                genome_median = keys_coverages[i]
-                return [genome_median, round(sum_of_coverages/line_counter, 2),
-                        keys_coverages[-1],
-                        keys_coverages[0]]
-    else:
-        half_sum_values_coverages = int(half_sum_values_coverages)
-        for i in range(len(keys_coverages)):
-            count += coverages_amounts_dict[keys_coverages[i]]
-            if count == half_sum_values_coverages:
-                genome_median = (keys_coverages[i] + keys_coverages[i+1])/2
-                return [genome_median, round(sum_of_coverages/line_counter, 2),
-                        keys_coverages[-1], keys_coverages[0]]
-            elif count > half_sum_values_coverages:
-                genome_median = keys_coverages[i]
-                return [genome_median, round(sum_of_coverages/line_counter, 2),
-                        keys_coverages[-1],
-                        keys_coverages[0]]
-
-
 def window_stats(coverages_amounts_dict: Counter) -> list:
     sum_of_coverages = 0
     for key, value in coverages_amounts_dict.items():
@@ -116,79 +58,80 @@ def pretty_printer(dataframe):
 
 def main():
     # created dataframe for whole genome and scaffolds stats
-    df_whole_and_scaffolds = pd.DataFrame(columns=['median', 'average', 'max', 'min'])
-    df_stacking_windows = pd.DataFrame(columns=['median', 'average', 'max', 'min'])
+    df_scaffolds = pd.DataFrame(columns=['median', 'average', 'max', 'min'])
+    df_frames = pd.DataFrame(columns=['median', 'average', 'max', 'min'])
+    df_whole_genome = pd.DataFrame(columns=['median', 'average', 'max', 'min'])
 
     genome_coverages_amounts_dict = Counter()
-    genome_coverage_amount = 0
     genome_line_counter = 0
     frame_coverages_amounts_dict = Counter()
-    frame_coverage_amount = 0
     frame_line_counter = 0
     frame_id = 0
     overlapping_frame_id = 0.5
     overlapping_frame_coverages_amounts_dict = Counter()
     overlapping_frame_line_counter = 0
-    overlapping_frame_coverage_amount = 0
-    scaffold_coverages_dict = defaultdict(list)
+    scaffold_coverages_dict = Counter()
 
     for line in args.input:
         # for whole genome
         line = line.rstrip().split('\t')
-
         genome_line_counter += 1
         frame_line_counter += 1
         frame_coverages_amounts_dict[int(line[2])] += 1
-        frame_coverage_amount += int(line[2])
+        scaffold_coverages_dict[int(line[2])] += 1
         genome_coverages_amounts_dict[int(line[2])] += 1
-        genome_coverage_amount += int(line[2])
-        if genome_line_counter >= int(args.frame_size / 2):
+        if genome_line_counter >= int(args.frame_size / 2): # !!!!!!!add args
             overlapping_frame_line_counter += 1
-            overlapping_frame_coverage_amount += int(line[2])
             overlapping_frame_coverages_amounts_dict[int(line[2])] += 1
 
         # for each scaffold
-        scaffold_coverages_dict[line[0]].append(int(line[2]))
-        if len(scaffold_coverages_dict.keys()) > 1:  # for each scaffold
-            df_whole_and_scaffolds.loc[scaffold_name] = scaffold_stats(scaffold_coverages_dict, scaffold_name)
+        try:
+            if previous_scaffold_name != line[0]:
+                df_scaffolds.loc[previous_scaffold_name] = window_stats(scaffold_coverages_dict)
+                scaffold_coverages_dict.clear()
+        except UnboundLocalError:
+            pass
 
-        # for each stacking window (non-overlapping)
+        # for window (non-overlapping)
         if frame_line_counter == args.frame_size:
             frame_id += 1
-            df_stacking_windows.loc['frame_'+str(frame_id)] = frame_stats(frame_coverages_amounts_dict, frame_line_counter)
-            frame_coverages_amounts_dict = Counter()
-            frame_coverage_amount = 0
+            df_frames.loc['frame_'+str(frame_id)] = window_stats(frame_coverages_amounts_dict)
+            frame_coverages_amounts_dict.clear()
             frame_line_counter = 0
 
-        # for each stacking window (overlapping)
+        # for window (overlapping)
         elif overlapping_frame_line_counter == args.frame_size:
             overlapping_frame_id += 1
-            df_stacking_windows.loc['frame_'+str(overlapping_frame_id)] = frame_stats(overlapping_frame_coverages_amounts_dict, overlapping_frame_line_counter)
-            overlapping_frame_coverages_amounts_dict = Counter()
-            overlapping_frame_coverage_amount = 0
+            df_frames.loc['frame_'+str(overlapping_frame_id)] = window_stats(overlapping_frame_coverages_amounts_dict)
+            overlapping_frame_coverages_amounts_dict.clear()
             overlapping_frame_line_counter = 0
 
-        scaffold_name = line[0]
+        previous_scaffold_name = line[0]
 
     # processing residual data after a cycle for each scaffold and whole genome
-    df_whole_and_scaffolds.loc[scaffold_name] = scaffold_stats(scaffold_coverages_dict, scaffold_name)
-    df_whole_and_scaffolds.loc['whole_genome'] = frame_stats(genome_coverages_amounts_dict, genome_line_counter)
+    df_scaffolds.loc[previous_scaffold_name] = window_stats(scaffold_coverages_dict)
+    df_whole_genome.loc['whole_genome'] = window_stats(genome_coverages_amounts_dict)
 
     #for print dataframe to terminal
-    df_whole_and_scaffolds = pretty_printer(df_whole_and_scaffolds)
-    df_stacking_windows = pretty_printer(df_stacking_windows)
+    df_scaffolds = pretty_printer(df_scaffolds)
+    df_frames = pretty_printer(df_frames)
+    df_whole_genome = pretty_printer(df_whole_genome)
 
-    if args.output:  # create a report.csv
-        df_whole_and_scaffolds.rename_axis('scaffold').reset_index().to_csv(args.output + "_whole_and_scaffolds.csv", encoding='utf-8', sep='\t')
-        df_stacking_windows.rename_axis('frame').reset_index().to_csv(args.output + "_stacking_windows.csv", encoding='utf-8', sep='\t')
+    if args.output: # create a report.csv
+        df_scaffolds.rename_axis('scaffold').reset_index().to_csv(args.output + "_scaffolds_stats.csv", encoding='utf-8', sep='\t')
+        df_frames.rename_axis('frame').reset_index().to_csv(args.output + "_windows_stats.csv", encoding='utf-8', sep='\t')
+        df_whole_genome.rename_axis('genome').reset_index().to_csv(args.output + "_whole_genome_stats.csv", encoding='utf-8', sep='\t')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="script for calculating median, average, maximum and minimum coverage in file.tab. Report to files.csv")
-    group_additional = parser.add_argument_group('Additional options')
-    group_additional.add_argument('-i', '--input', type=lambda s: metaopen(s, "rt"),
+
+    group_required = parser.add_argument_group('Required options')
+    group_required.add_argument('-i', '--input', type=lambda s: metaopen(s, "rt"),
                                   help="input file.bam.gz (don`t use for STDIN)", default=stdin)
+
+    group_additional = parser.add_argument_group('Additional options')
     group_additional.add_argument('-f', '--frame-size', type=int,
                                   help="calculate stats in 100 kbp and 1 Mbp stacking windows", default=500)
     group_additional.add_argument('-o', '--output', metavar='PATH', type=str,
