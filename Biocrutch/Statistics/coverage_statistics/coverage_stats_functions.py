c31273af46f22d6974650fd4e197583b21c0d080
etozhetoma <etozhetoma@ya.ru>
2020-10-10 11:32:15 +0300

    median fixes

--- Biocrutch/Statistics/coverage_statistics/coverage_stats_functions.py
+++ Biocrutch/Statistics/coverage_statistics/coverage_stats_functions.py
@@ -7,38 +7,37 @@ def count_window_stats(coverages_amounts_dict: Counter) -> list:
     Function for calculating median, average, maximum and minimum coverage.
     Works with a Counter from collections packege.
     Output is a list with metrics.
+    Подсчет медианы можно перефразировать как:
+1. в случае нечетного числа значений - нахождение покрытия на позиции int(N/2)
+2. в случае четного - нахождение покрытия на позиции N/2 - 1 и N/2 и взятие среднего от них
+N - общее число значений. номера позиций(индексы в сортированном списке всех значений) - 0-based (то есть начинаются с нуля)
     '''
     sum_of_coverages = sum(key*value for key, value in coverages_amounts_dict.items())
+    sum_values_coverages = sum(coverages_amounts_dict.values())
+    mean = round(sum_of_coverages/sum_values_coverages, 2)
 
     keys_coverages = sorted(coverages_amounts_dict.keys())
-    sum_values_coverages = sum(coverages_amounts_dict.values())
-    half_sum_values_coverages = sum_values_coverages / 2
+    half_sum_values_coverages = sum_values_coverages // 2
     count = 0
 
     if sum_values_coverages % 2 != 0:
-        for i in range(len(keys_coverages)):
-            count += coverages_amounts_dict[keys_coverages[i]]
+        for key in keys_coverages:
+            count += coverages_amounts_dict[key]
             if count >= half_sum_values_coverages:
-                genome_median = keys_coverages[i]
-                metrics = [genome_median, round(sum_of_coverages/sum(coverages_amounts_dict.values()), 2),
-                        keys_coverages[-1],
-                        keys_coverages[0]]
+                median = key
+                metrics = [median, mean, keys_coverages[-1], keys_coverages[0]]
                 print('window_stats: ', metrics)
                 return metrics
     else:
-        half_sum_values_coverages = int(half_sum_values_coverages)
         for i in range(len(keys_coverages)):
             count += coverages_amounts_dict[keys_coverages[i]]
             if count == half_sum_values_coverages:
                 genome_median = (keys_coverages[i] + keys_coverages[i+1])/2
-                metrics = [genome_median, round(sum_of_coverages/sum(coverages_amounts_dict.values()), 2),
-                        keys_coverages[-1], keys_coverages[0]]
+                metrics = [genome_median, mean, keys_coverages[-1], keys_coverages[0]]
                 print('window_stats: ', metrics)
                 return metrics
             elif count > half_sum_values_coverages:
                 genome_median = keys_coverages[i]
-                metrics = [genome_median, round(sum_of_coverages/sum(coverages_amounts_dict.values()), 2),
-                        keys_coverages[-1],
-                        keys_coverages[0]]
+                metrics = [genome_median, mean, keys_coverages[-1], keys_coverages[0]]
                 print('window_stats: ', metrics)
                 return metrics
