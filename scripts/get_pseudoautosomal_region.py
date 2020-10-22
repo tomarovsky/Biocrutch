5239c2e37c24a0d37f04a39fa2a5df36f3c73b4f
etozhetoma <etozhetoma@ya.ru>
2020-10-22 17:34:02 +0300

    added get_pseudoautosomal_region.py

---
+++ scripts/get_pseudoautosomal_region.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+__author__ = 'tomarovsky'
+'''
+Script for determining the coordinates of the pseudoautosomal region.
+Output of coordinates to BED file.
+'''
+from Biocrutch.Routines.routine_functions import metaopen
+from sys import stdin
+import pandas as pd
+import argparse
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script for determining the coordinates of the pseudoautosomal region. Output of coordinates to BED file.")
+
+    group_required = parser.add_argument_group('Required options')
+    group_required.add_argument('-i', '--input', type=lambda s: metaopen(s, "rt"),
+                                  help="input file.bam.gz (don`t use for STDIN)", default=stdin)
+
+    group_additional = parser.add_argument_group('Additional options')
+    group_additional.add_argument('-f', '--frame-size', type=int,
+                                  help="calculate stats in 100 kbp and 1 Mbp stacking windows", default=100000)
+    group_additional.add_argument('--coverage_column_name', type=str,
+                                  help="Name of column in coverage file with mean/median coverage per window", default="median")
+    group_additional.add_argument('-s', '--scaffold-name', type=str,
+                                  help="Name of column in coverage file with scaffold name", default="scaffold")
+    group_additional.add_argument('-m', '--median', type=int,
+                                  help="whole genome median/mean value", default=100000)
