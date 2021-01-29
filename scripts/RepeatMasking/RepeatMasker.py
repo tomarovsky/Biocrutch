3421622b58ddfb50689b7d96be229a54b41c6980
tomarovsky <andrey.tomarovsky@gmail.com>
2021-01-29 11:22:36 +0300

    fixes

--- scripts/RepeatMasking/RepeatMasker.py
+++ scripts/RepeatMasking/RepeatMasker.py
@@ -5,8 +5,14 @@ import argparse
 from sys import stdin
 
 def main():
-    count = 0
+    if args.input == stdin:
+        outprefix = "repeatmasker.out"
+    else:
+        outprefix = ('.').join(args.input.split('.')[:-1])
+    outfile = metaopen (outprefix + ".gff", "w")
+    
     with metaopen(args.input, "r", buffering=args.buffering) as data:
+        count = 0
         for l in data:
             if count < 4:
                 count += 1
@@ -19,9 +25,8 @@ def main():
             additional_info = "Class=%s;Family=%s;Matching_repeat=%s;SW_score=%s;Perc_div=%s;Perc_del=%s;Pers_ins=%s"\
                                % (class_family_info[0], class_family_info[1],
                                   line[9], line[0], line[1], line[2], line[3])
-            gff = "%s\tRepeatMasker\trepeat\t%s\t%s\t.\t%s\t.\t%s\n" % (line[4], line[5], line[6], strand, additional_info)
-            print(gff)
-            
+            gff_line = "%s\tRepeatMasker\trepeat\t%s\t%s\t.\t%s\t.\t%s\n" % (line[4], line[5], line[6], strand, additional_info)
+            outfile.write(gff_line)
 
 
 if __name__ == "__main__":
