3449cbd0b89eb4db6e8a5a3b52f305e83b9d7ed5
tomarovsky <andrey.tomarovsky@gmail.com>
2021-04-26 21:20:08 +0300

    fix

--- scripts/Coverage/kmer_coverage_for_circos_visualization.py
+++ scripts/Coverage/kmer_coverage_for_circos_visualization.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
 __author__ = 'tomarovsky'
 
-from Biocrutch.Routines.routine_functions import metaopen, metaoutput
 import argparse
 from sys import stdin
+from collections import Counter
+from Biocrutch.Routines.routine_functions import metaopen, metaoutput
+from Biocrutch.Statistics.coverage_statistics.CoverageMetrics import CoveragesMetrics
 
 
 def main():
     outfile = metaopen(metaoutput(args.output, ".csv"), "wt")
-    for line in args.input:
-        scaffold_name, start, stop, coverage_value = args.input.readline().strip().split()
-        coverage_value = int(coverage_value)
-        if (coverage_value > (2.5 * args.whole_median)) or coverage_value < (0.5 * args.whole_median):
-            break
+    frame_coverages_amounts_dict = Counter()
+    line_counter = 0
+    frame_line_counter = 0
 
-    for i in args.input:
-        line = i.strip().split()
-        line[3] = int(line[3])
-        if (line[3] > (2.5 * args.whole_median)) or (line[3] < (0.5 * args.whole_median)):
-            if line[0] == scaffold_name:
-                if stop == line[1]:
-                    stop = line[2]
-                    continue
-                else:
-                    outfile.write("\t".join([scaffold_name, start, stop]) + "\n")
-                    start = line[1]
-                    stop = line[2]
-            else:
-                outfile.write("\t".join([scaffold_name, start, stop]) + "\n")
-                scaffold_name, start, stop = line[:3]
-    outfile.write("\t".join([scaffold_name, start, stop]) + "\n")
-
+    for ln in args.input:
+        line = ln.strip().split()
+        line_counter += 1
+        frame_line_counter += 1
+        frame_coverages_amounts_dict[int(line[1])] += 1
+        if frame_line_counter == args.frame_size:
+            start = line_counter - args.frame_size + 1
+            stop = line_counter
+            metrics = CoveragesMetrics(frame_coverages_amounts_dict)
+            coverage = metrics.median_value()
+            outfile.write("\t".join(["MT", str(start), str(stop), str(coverage)]) + "\n")
+            frame_coverages_amounts_dict.clear()
+            frame_line_counter = 0
+    if frame_coverages_amounts_dict:
+        start = line_counter - sum(frame_coverages_amounts_dict.values()) + 1
+        stop = line_counter
+        metrics = CoveragesMetrics(frame_coverages_amounts_dict)
+        coverage = metrics.median_value()
+        outfile.write("\t".join(["MT", str(start), str(stop), str(coverage)]) + "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="get kmers coverage statistics for circos")
     group_required = parser.add_argument_group('Required options')
     group_required.add_argument('-i', '--input', type=lambda s: metaopen(s, "rt"),
                                 help="input splited_fasta_1.fasta", default=stdin)
+    group_required.add_argument('-k', '--kmer-length', type=int,
+                                help="kmer length", default=23)
+    group_required.add_argument('-f', '--frame-size', type=int,
+                                help="non-overlapping frame size", default=10)
     group_required.add_argument('-o', '--output', type=str,
                                 help="output file prefix")
     args = parser.parse_args()
