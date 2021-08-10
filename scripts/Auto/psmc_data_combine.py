fd456bb027512c6bd734fb3235dd349d1f1b133a
tomarovsky <andrey.tomarovsky@gmail.com>
2021-08-10 22:20:18 +0300

    PSMC data combine added

---
+++ scripts/Auto/psmc_data_combine.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+__author__ = 'tomarovsky'
+from argparse import ArgumentParser
+
+def main():
+    result = []
+
+    with open(args.input[0], "r") as f:
+        for i in f:
+            if len(i) > 2:
+                axis_x, axis_y = float(i.split()[0]), float(i.split()[1])
+                tmp = [axis_x, axis_y, "", "", ""]
+                result.append(tmp)
+            else:
+                result.append("")
+
+    with open(args.input[1], "r") as f:
+        for i in f:
+            if len(i) > 2:
+                axis_x, axis_y = float(i.split()[0]), float(i.split()[1])
+                tmp = [axis_x, "", axis_y, "", ""]
+                result.append(tmp)
+            else:
+                result.append("")
+
+    with open(args.input[2], "r") as f:
+        for i in f:
+            if len(i) > 2:
+                axis_x, axis_y = float(i.split()[0]), float(i.split()[1])
+                tmp = [axis_x, "", "", axis_y, ""]
+                result.append(tmp)
+            else:
+                result.append("")
+            
+    with open(args.input[3], "r") as f:
+        for i in f:
+            if len(i) > 2:
+                axis_x, axis_y = float(i.split()[0]), float(i.split()[1])
+                tmp = [axis_x, "", "", "", axis_y]
+                result.append(tmp)
+            else:
+                result.append("")
+
+    flag = False
+    with open(args.output, "a") as res:
+        for i in result:
+            if len(i)> 1:
+                if i[0] != 0.0:
+                    line = "\t".join(map(str, i)).replace(".", ",")
+                    # print(line)
+                    res.write(line)
+                    res.write("\n")
+                    flag = None
+                else:
+                    if flag != False:
+                        res.write("\n")
+                        flag = True
+                    line = "\t".join(map(str, i)).replace(".", ",")
+                    # print(line)
+                    res.write(line)
+                    res.write("\n")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="script for combining PSMC data into one table for visualization")
+    group_required = parser.add_argument_group('Required options')
+    group_required.add_argument('-i', '--input', type=str,
+                                nargs='+', help="PSMC files (first rounds(1 and 2), then diploids(1 and 2)!)")
+    group_required.add_argument('-o', '--output', type=str, help="outfile name", default="PSMC.results.tab")
+    args = parser.parse_args()
+    main()
