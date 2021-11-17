95f858efb53a4333a3e8f79d1d37d434857ab1c4
Tomarovsky <andrey.tomarovsky@gmail.com>
2021-11-17 13:58:06 +0300

    fix

--- scripts/Coverage/coverage_masking.py
+++ scripts/Coverage/coverage_masking.py
@@ -2,9 +2,9 @@
 __author__ = 'tomarovsky'
 '''
 Get coverage mask. You can use pipeline:
-zcat SAMPLE.per-base.bed.gz | awk '{if( ($4 > 2.5 * whole_genome_median) || ($4 < 0.5 * whole_genome_median) print $0)}' | covarege_masking.py
+zcat SAMPLE.per-base.bed.gz | awk '{if( ($4 > 2.5 * whole_genome_median) || ($4 < 0.5 * whole_genome_median) print $0)}' | coverage_masking.py
 WHOLE_MEDIAN=$(cat *_whole_genome_stats.csv | sed -n 2p | awk '{print $2}');
-zcat SAMPLE.per-base.bed.gz | awk '{if( ($4 > 2.5 * $WHOLE_MEDIAN) || $4 < 0.5 * $WHOLE_MEDIAN) print $0)}' | covarege_masking.py
+zcat SAMPLE.per-base.bed.gz | awk '{if( ($4 > 2.5 * $WHOLE_MEDIAN) || $4 < 0.5 * $WHOLE_MEDIAN) print $0)}' | coverage_masking.py
 '''
 from Biocrutch.Routines.routine_functions import metaopen, metaoutput
 import argparse
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="get the coverage mask through the pipeline")
     group_required = parser.add_argument_group('Required options')
     group_required.add_argument('-i', '--input', type=lambda s: metaopen(s, "rt"),
-                                help="input coverage_statistics_output.csv (don`t use for STDIN)", default=stdin)
+                                help="input coverage_statistics_output.csv)", default=stdin)
     group_required.add_argument('-w', '--whole-median', type=float,
                                 help="whole median value")
     group_required.add_argument('-o', '--output', type=str,
