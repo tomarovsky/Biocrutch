2908f67ac27d4ec443afcb8691757265bf019772
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-19 23:46:22 +0300

    test

--- scripts/SRA_toolkit.py
+++ scripts/SRA_toolkit.py
@@ -6,6 +6,7 @@ from Biocrutch.Parsers.url_parsers import SRA_download_link
 from Biocrutch.Parsers.url_parsers import SRA_metrics
 
 
+
 def main():
     for SRA_id in args.input:
         if args.download:
