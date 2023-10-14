cb28c631c9ff2f8eb1b4efef8b1fe3dc18f76451
Tomarovsky <andrey.tomarovsky@gmail.com>
2023-10-14 06:13:01 +0300

    exclude option added

--- scripts/ROH/get_ROH_regions.py
+++ scripts/ROH/get_ROH_regions.py
@@ -42,7 +42,6 @@ def merge_regions_by_distance(df, min_distance):
                     result[-1].append(df.iloc[i]["end"])
     roh_df = pd.DataFrame(result, columns=['scaffold', 'start', 'end'])
     roh_df['length'] = roh_df['end'] - roh_df['start']
-    print(roh_df)
     return roh_df
 
 
@@ -50,6 +49,10 @@ def main():
     df = pd.read_csv(args.input, sep="\t")
     df = filter_by_level_of_heterozygosity(df, args.first_threshold, args.second_threshold, args.max_adjacent_windows)
     roh_df = merge_regions_by_distance(df, args.min_distance)
+    if args.exclude_scaffold_list:
+        for scaffold in args.exclude_scaffold_list:
+            roh_df = roh_df[roh_df['scaffold'] != scaffold]
+    print(roh_df)
     roh_df.to_csv(args.output, sep='\t', index=False)
 
 if __name__ == "__main__":
@@ -58,10 +61,16 @@ if __name__ == "__main__":
     group_required.add_argument('-i', '--input', type=str, help="MACE BED file")
     group_required.add_argument('-o', '--output', type=str, help="outfile name")
     group_additional = parser.add_argument_group('Additional options')
-    group_additional.add_argument('-f', '--first_threshold', type=int, default=0.05, help="lower threshold for filtering by adjacent windows")
-    group_additional.add_argument('-s', '--second_threshold', type=int, default=0.10, help="max (in ascending order of heterozygosity value) threshold")
-    group_additional.add_argument('-a', '--max_adjacent_windows', type=int, default=5, help="max value of adjacent windows")
-    group_additional.add_argument('-d', '--min_distance', type=int, default=50000, help="min distance for merging row ROHs")
+    group_additional.add_argument('-f', '--first_threshold', type=int, default=0.05,
+                                  help="lower threshold for filtering by adjacent windows")
+    group_additional.add_argument('-s', '--second_threshold', type=int, default=0.10,
+                                  help="max (in ascending order of heterozygosity value) threshold")
+    group_additional.add_argument('-a', '--max_adjacent_windows', type=int, default=5,
+                                  help="max value of adjacent windows")
+    group_additional.add_argument('-d', '--min_distance', type=int, default=50000,
+                                  help="min distance for merging row ROHs")
+    group_additional.add_argument('-x', '--exclude_scaffold_list', type=lambda s: s.split(","), default=False,
+                                  help="Comma-separated list of excluded scaffolds")
     args = parser.parse_args()
     main()
 
