a536ced711904a988ad19369f23af27204da4341
Tomarovsky <andrey.tomarovsky@gmail.com>
2023-11-10 11:06:32 +0300

    fix

--- scripts/Auto/svg2png.py
+++ scripts/Auto/svg2png.py
@@ -2,6 +2,7 @@
 __author__ = 'tomarovsky'
 import cairosvg
 import sys
+import os
 
 def convert_svg_to_png(input_svg_path, output_png_path, scale_factor=3):
     cairosvg.svg2png(url=input_svg_path, 
@@ -9,13 +10,13 @@ def convert_svg_to_png(input_svg_path, output_png_path, scale_factor=3):
                      scale=scale_factor)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print("Usage: python script_name.py SVG PNG Scale")
         sys.exit(1)
 
     input_svg_path = sys.argv[1]
-    output_png_path = sys.argv[2]
-    scale_factor = float(sys.argv[3])
+    output_png_path = os.path.splitext(input_svg_path)[0] + ".png"
+    scale_factor = float(sys.argv[2])
 
     convert_svg_to_png(input_svg_path, output_png_path, scale_factor)
 
