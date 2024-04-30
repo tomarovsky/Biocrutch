6082e0a2fdebbc76098e862c9a286c58023d9df0
Tomarovsky <andrey.tomarovsky@gmail.com>
2024-04-30 06:48:48 +0300

    fix statistics

--- scripts/Statistics/variant_counts.stats.py
+++ scripts/Statistics/variant_counts.stats.py
@@ -9,18 +9,24 @@ def main():
         df = pd.read_csv(file, sep='\t', header=0)
         sample = df.columns[2]
 
-        # Calculate mean, median and mode
+        df[sample] = df[sample] / args.window_size * args.multiplicator
+
+        maxim = df[sample].max()
+        minim = df[sample].min()
         mean = df[sample].mean()
         median = df[sample].median()
         mode = df[sample].mode()[0]
 
-        print(f"{sample}\t{mean}\t{median}\t{mode}")
+        # print("sample\tmax\tmin\tmean\tmedian\tmode")
+        print(f"{sample}\t{maxim}\t{minim}\t{mean:.3f}\t{median}\t{mode}")
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="script to calculate mean, median and mode based .variant_counts.tsv")
+    parser = ArgumentParser(description="script to calculate max, min, mean, median and mode based .variant_counts.tsv (SNPs/1kbp)")
     group_required = parser.add_argument_group('Required options')
     group_required.add_argument('-i', '--input', type=str, nargs='+', help="MACE BED file")
+    group_required.add_argument('-w', '--window_size', type=int, help="Window size", default=1000000)
+    group_required.add_argument('-m', '--multiplicator', type=int, help="Multiplicator", default=1000)
     # group_additional = parser.add_argument_group('Additional options')
     # group_additional.add_argument('-o', '--output', type=str, default=False, help="outfile name")
     # group_additional.add_argument('-h', '--header', type=bool, default=False, help="write header to outfile")
