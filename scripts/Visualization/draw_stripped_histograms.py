d8eea44a1ecaa6814744ecb9e32b74a4f3ebbdaa
Tomarovsky <andrey.tomarovsky@gmail.com>
2024-07-29 13:52:24 +0300

    add draw_stripped_histogram.py

--- scripts/Visualization/draw_stripped_histograms.py
+++ scripts/Visualization/draw_stripped_histograms.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 __author__ = 'tomarovsky'
-import argparse
 from collections import OrderedDict
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
 
 
-def load_and_prepare_data(file_dict, label, args):
+def load_and_prepare_data(file_dict, args):
     data_list = []
     for entry in file_dict:
         df = pd.read_csv(file_dict[entry], sep="\t")
@@ -15,63 +16,75 @@ def load_and_prepare_data(file_dict, label, args):
             df = df[~df["CHROM"].str.contains("chrX")]
         df.set_index("CHROM", inplace=True)
         density = df.iloc[:, -1] / args.window_size * args.multiplicator
-        data_list.extend([{'density': d, 'id': entry, 'Reference': label} for d in density])
+        data_list.extend([{'density': d, 'id': entry} for d in density])
     return data_list
 
 
+def violin_to_poligon(ax, data, bins, x_offset, color, edgecolor, label=None):
+    hist, bin_edges = np.histogram(data, bins=bins, density=True)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    bin_centers = np.insert(bin_centers, 0, 0)
+    hist = np.insert(hist, 0, hist[0])
+    bin_centers = np.insert(bin_centers, 0, 0)
+    hist = np.insert(hist, 0, 0)
+    bin_centers = np.append(bin_centers, bin_centers[-1])
+    hist = np.append(hist, 0)
+    scaling_factor = 0.45 / max(hist, default=1)
+    hist *= scaling_factor
+
+    ax.fill_betweenx(bin_centers, x_offset, x_offset - hist,
+                     color=color, linewidth=0.4, edgecolor=edgecolor, zorder=2)
+    ax.fill_betweenx(bin_centers, x_offset, x_offset + hist,
+                     color=color, linewidth=0.4, edgecolor=edgecolor, zorder=2, label=label)
+
+
 def main():
+    if args.darkgrid:
+        sns.set_style("darkgrid", {"axes.facecolor": "#efefefff"})
+    else:
+        plt.rcParams.update({'font.size': args.font_size})
+
     plt.rcParams.update({'font.size': args.font_size})
 
-    with open(args.input1, "r") as in_fd:
-        file_dict1 = OrderedDict([line.strip().split("\t") for line in in_fd])
+    with open(args.input, "r") as in_fd:
+        file_dict = OrderedDict([line.strip().split("\t") for line in in_fd])
 
-    with open(args.input2, "r") as in_fd:
-        file_dict2 = OrderedDict([line.strip().split("\t") for line in in_fd])
+    data = load_and_prepare_data(file_dict, args)
+    result = pd.DataFrame(data)
 
-    data1 = load_and_prepare_data(file_dict1, args.legend_labels_list[0], args)
-    data2 = load_and_prepare_data(file_dict2, args.legend_labels_list[1], args)
-
-    result = pd.DataFrame(data1 + data2)
     if args.only_count:
         result.to_csv("counts.csv", index=False)
 
+    fig, ax = plt.subplots(figsize=(args.figure_width_per_sample * len(file_dict), args.figure_height), dpi=args.dpi)
+    plt.xticks(rotation=args.rotation)
 
-    fig, ax = plt.subplots(figsize=(args.figure_width_per_sample * len(file_dict1), args.figure_height), dpi=args.dpi)
-    plt.xticks(rotation=args.rotation, ha='right')
+    unique_ids = result['id'].unique()
+    for i, unique_id in enumerate(unique_ids):
+        df = result[result['id'] == unique_id]['density']
 
-    sns.violinplot(data=result,
-                   x="id",
-                   y="density",
-                   hue="Reference",
-                   split = True,
-                   density_norm='width',
-                   inner='box',
-                   inner_kws=dict(box_width=1.5,
-                                  whis_width=0,
-                                  marker = '.',
-                                  markersize=2.2,
-                                  markeredgewidth=0.6),
-                   linewidth=0.5,
-                   saturation=1,
-                   palette=args.colors_list)
+        # bins
+        bin_width = 0.1
+        bins = np.arange(df.min(), df.max() + bin_width, bin_width)
 
-    ax.set_xticks(range(len(file_dict1.keys())))
-    ax.set_xticklabels(file_dict1.keys())
+        violin_to_poligon(ax, df, bins, i, '#E04B4B', '#575757', label='M. zibellina' if i == 0 else None)
+
+        # boxplot
+        ax.boxplot(df, vert=True, positions=[i], showfliers=False, medianprops=dict(color='white'))
+
+    plt.xticks(range(len(unique_ids)), unique_ids, ha='right', rotation=45)
     plt.yticks(args.yticklist)
     if args.horizontal_lines:
         for ycoord in args.horizontal_lines:
-            plt.axhline(y=ycoord, color="#CC0000", linestyle="--", linewidth=1)
+            plt.axhline(y=ycoord, color="red", linestyle="--", linewidth=1)
     plt.ylim(ymax=args.ymax, ymin=args.ymin)
-    plt.subplots_adjust(left=args.subplots_adjust_left,
-                        right=args.subplots_adjust_right,
-                        top=args.subplots_adjust_top,
-                        bottom=args.subplots_adjust_bottom)
+    plt.xlim(-0.5, len(unique_ids) - 0.5)
+
+    plt.subplots_adjust(left=args.subplots_adjust_left, right=args.subplots_adjust_right,
+                        top=args.subplots_adjust_top, bottom=args.subplots_adjust_bottom)
     if args.figure_grid:
-        plt.grid(color="gray", linestyle = '--', linewidth = 1.5)
-    ax.set_ylabel(args.ylabel)
-    ax.set_xlabel('')
+        plt.grid(color="gray", linestyle = '--', linewidth = 0.5)
+    plt.ylabel(args.ylabel)
     plt.title(args.title)
-    plt.legend(loc='upper left', title='Reference', ncol = 2)
     plt.tight_layout()
     for ext in args.output_formats:
         plt.savefig("{0}.{1}".format(args.output_prefix, ext))
@@ -79,18 +92,9 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input1", required=True,
-                        help="Input file with two columns containing label in the first one and filename in the second.")
-    parser.add_argument("--input2", required=True,
-                        help="Input file with two columns containing label in the first one and filename in the second.")
-    parser.add_argument("--legend_labels_list", action="store", dest="legend_labels_list", type=lambda s: s.split(","),
-                        default=("Species_1", "Species_2"),
-                        help="Comma-separated list of species labels of output figure (for input1 and input2, respectively)."
-                        "Default: Species_1, Species_2")
-    parser.add_argument("--colors_list", action="store", dest="colors_list", type=lambda s: s.split(","),
-                        default=('#6094C3', '#E04B4B'),
-                        help="Comma-separated list of colors of output figure (for input1 and input2, respectively)."
-                        "Default: #6094C3 (blue), #E04B4B (red)")
+    parser.add_argument("-i", "--input", required=True,
+                        help="Input file with two columns containing label in the first one and filename in the second."
+                             "Boxplots will be drawn in the same order as labels")
     parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                         help="Prefix of output files")
     parser.add_argument("-d", "--dpi", action="store", dest="dpi", type=int, default=300,
@@ -98,16 +102,15 @@ if __name__ == '__main__':
 
     parser.add_argument("--figure_height", action="store", dest="figure_height", type=float, default=6.0,
                         help="Height of figure in inches. Default: 6")
-    parser.add_argument("--figure_width_per_sample", action="store", dest="figure_width_per_sample", type=float, default=0.5,
-                        help="Per sample width of figure in inches. Default: 0.5")
+    parser.add_argument("--figure_width_per_sample", action="store", dest="figure_width_per_sample", type=float, default=1,
+                        help="Per sample width of figure in inches. Default: 1")
     parser.add_argument("--figure_grid", action="store_true", default=False,
                         help="Add grid lines to the figure. Default: False")
     parser.add_argument("--font-size", action="store", dest="font_size", type=float, default=16,
                         help="Font size. Default: 16")
     parser.add_argument("-e", "--output_formats", action="store", dest="output_formats", type=lambda s: s.split(","),
-                        default=("svg", "png"),
-                        help="Comma-separated list of formats (supported by matlotlib) of "
-                             "output figure.Default: svg,png")
+                        default=("png", "svg"), help="Comma-separated list of formats (supported by matlotlib) of "
+                             "output figure.Default: png,svg")
 
     parser.add_argument("-l", "--title", action="store", dest="title", default="Variant density",
                         help="Suptitle of figure. Default: 'Variant density'")
@@ -128,7 +131,6 @@ if __name__ == '__main__':
                              "Default: 0.05,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.25,1.5,2,3,4,5")
     parser.add_argument("--rotation",  action="store", dest="rotation", type=float, default=90,
                         help="Rotation angle for X labels. Default: 90")
-
     parser.add_argument("--horizontal_lines",  action="store", dest="horizontal_lines",
                         type=lambda s: list(map(float, s.split(","))),
                         help="Comma-separated list of y-coordinates to draw horizontal lines. "
@@ -146,10 +148,10 @@ if __name__ == '__main__':
                         help="Do not use counts from X chromosome. Default: False")
     parser.add_argument("--only_count", action="store_true", dest="only_count", default=False,
                         help="Only count and save variants to CSV, do not draw them. Default: False")
+    parser.add_argument("--darkgrid", action="store_true", dest="darkgrid", default=False,
+                        help="Pretty visualization using theme 'darkgrid' from Seaborn. Default: False")
 
     args = parser.parse_args()
-
     main()
-
 
 
