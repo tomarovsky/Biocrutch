a5763ef75029f1d91b51e5e6370ef531d9d135d4
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-04-20 06:59:19 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-__author__ = 'tomarovsky'
+__author__ = "tomarovsky"
 
 from pathlib import Path
 from os.path import join, dirname
 from setuptools import setup, find_packages
 
-dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'biopython',
-                'lxml', 'beautifulsoup4', 'requests']
+dependencies = ["scipy", "numpy", "pandas", "matplotlib", "biopython", "lxml", "beautifulsoup4", "requests"]
 
-setup(name='Biocrutch',
-      version='0.1',
-      packages=find_packages(),
-      author='Andrey Tomarovsky',
-      author_email='andrey.tomarovsky@gmail.com',
-      install_requires=dependencies,
-      long_description=open(join(dirname(__file__), 'README.md')).read(),
-      scripts=list(map(str, sorted(Path('scripts/').rglob("*.py")))))
+setup(
+    name="Biocrutch",
+    version="0.1",
+    packages=find_packages(),
+    author="Andrey Tomarovsky",
+    author_email="andrey.tomarovsky@gmail.com",
+    install_requires=dependencies,
+    long_description=open(join(dirname(__file__), "README.md")).read(),
+    scripts=list(map(str, sorted(Path("scripts/").rglob("*.py")))),
+)
