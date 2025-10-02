0a255bcccff9b95868a9e7dda056a79ed1842ef6
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-02 09:41:25 +0300

    first

---
+++ scripts/Convert/intersect_wao_groupby.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+__author__ = 'tomarovsky'
+
+import pandas as pd
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python  script.py  <input from bedtools intersect -wao>  <output_file>")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+df = pd.read_csv(input_file, sep='\s+', header=None)
+df.columns = ['scaffold1', 'start', 'end', 'scaffold2', 'start2', 'end2', 'value']
+
+result = df.groupby(['scaffold1', 'start', 'end'])['value'].sum().reset_index()
+
+result.to_csv(output_file, sep='\t', index=False, header=False)
